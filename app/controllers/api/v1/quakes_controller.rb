d71faf5c722e287aecfc8e4af2bc3c801212f6f1
Matt Watkajtys <watkajtys@gmail.com>
2013-08-25 17:54:28 -0400

    Only passing daily quakes on quakes controller.

--- app/controllers/api/v1/quakes_controller.rb
+++ app/controllers/api/v1/quakes_controller.rb
@@ -2,7 +2,7 @@ class Api::V1::QuakesController < ApplicationController
    respond_to :json
 
    def index
-      @quakes = Quake.order_by(:time => :desc)
+      @quakes = Quake.day.order_by(:time => :desc)
       respond_with @quakes
    end
 
