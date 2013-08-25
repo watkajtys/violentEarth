5e83f6dae59f6954e86b988ca75c9b578e10395b
Matt Watkajtys <watkajtys@gmail.com>
2013-08-25 16:45:01 -0400

    Removing saving from quakes controller.
    Not needed.

--- app/controllers/quakes_controller.rb
+++ app/controllers/quakes_controller.rb
@@ -6,11 +6,6 @@ class QuakesController < ApplicationController
 
 	def create
 		@quake = Quake.new params[:quake]
-		if @quake.save
-			redirect_to [:root], notice: "saved"
-		else
-			redirect_to [:root], alert: @quake.errors.full_messages
-		end
 	end
 
 end
